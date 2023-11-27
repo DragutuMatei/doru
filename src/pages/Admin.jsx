30e4101921e785e688e6651c105fcfc162af4360
matei <mateidr7@gmail.com>
2023-11-28 00:25:12 +0200

    ASd

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -53,12 +53,21 @@
 
 import React, { useState, useEffect } from "react";
 import Fire from "../Fire";
+import "../css/admin.scss";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const YourReactComponent = () => {
   const [data, setData] = useState([]);
   const [newDataKey, setNewDataKey] = useState(null);
 
   useEffect(() => {
+    // CKEDITOR.replace("post_text", {
+    //   language: "en",
+    //   uiColor: "#dddddd",
+    //   height: 500,
+    //   resize_dir: "vertical",
+    // });
     const fetchData = async () => {
       try {
         const dbHandler = new Fire();
@@ -121,33 +130,136 @@ const YourReactComponent = () => {
     }
   };
   const [name, setName] = useState("");
-  return (
-    <div>
-      <h1>Your React Component</h1>
-      <p>Fetched Data: {JSON.stringify(data)}</p>
-      <p>Newly Created Data Key: {newDataKey}</p>
-      <button onClick={handleCreateData}>Create Data</button>
-      <button onClick={handleUpdateData}>Update Data</button>
-      <input
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      {data != null &&
-        Object.entries(data) &&
-        Object.entries(data).map((da) => {
-          console.log(da[0] + "+" + da[1].name);
+  const [textEditor, setTextEditor] = useState([]);
 
-          return (
-            <>
-              <h1>{da[1].name}</h1>
-              <button onClick={() => handleDeleteData(da[0])}>
-                Delete Data
-              </button>
-            </>
+  return (
+    <>
+      <div>
+        <h1>Your React Component</h1>
+        <p>Fetched Data: {JSON.stringify(data)}</p>
+        <p>Newly Created Data Key: {newDataKey}</p>
+        <button onClick={handleCreateData}>Create Data</button>
+        <button onClick={handleUpdateData}>Update Data</button>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        {data != null &&
+          Object.entries(data) &&
+          Object.entries(data).map((da) => {
+            console.log(da[0] + "+" + da[1].name);
+
+            return (
+              <>
+                <h1>{da[1].name}</h1>
+                <button onClick={() => handleDeleteData(da[0])}>
+                  Delete Data
+                </button>
+              </>
+            );
+          })}
+      </div>
+      {/* <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js"
+      ></script> */}
+      {/* <div className="col-lg-12 page-main">
+        <div className="new-post">
+          <input
+            type="text"
+            className="title-input"
+            placeholder="Post title"
+            id="post_title"
+            name="post_title"
+          />
+
+          <textarea id="post_text" className="post-area"></textarea>
+
+          <button id="add_btn" className="add-btn">
+            <i className="fa fa-plus"></i> Add Post
+          </button>
+        </div>
+      </div> */}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from CKEditor 5!</p>"
+        config={{
+          toolbar: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "blockQuote",
+            "ckfinder",
+            "|",
+            "imageTextAlternative",
+            "imageUpload",
+            "imageStyle:full",
+            "imageStyle:side",
+            "|",
+            "mediaEmbed",
+            "insertTable",
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "|",
+            "undo",
+            "redo",
+          ],
+        }}
+        onInit={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+          console.log(
+            "toolbar: ",
+            Array.from(editor.ui.componentFactory.names())
           );
-        })}
-    </div>
+          console.log(
+            "plugins: ",
+            ClassicEditor.builtinPlugins.map((plugin) => plugin.pluginName)
+          );
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log(data);
+          setTextEditor(data);
+        }}
+        onBlur={(editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(editor) => {
+          console.log("Focus.", editor);
+        }}
+        onReady={(e) => {
+          console.log("GATAAA: ", e);
+        }}
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="wrap-result">
+        <div dangerouslySetInnerHTML={{ __html: textEditor }}></div>
+
+        {/* {textEditor && textEditor.map((item, id) => {
+        return (
+          <div key={id} className="result-card">
+            <button onClick={()=>Delete(item.id)}>Delete</button>
+          </div>
+        );
+      })} */}
+      </div>
+    </>
   );
 };
 
